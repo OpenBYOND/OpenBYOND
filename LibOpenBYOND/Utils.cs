c0aae9978d96522c97be7b9b14a134d1d0412cf1
Rob Nelson <nexisentertainment@gmail.com>
2014-08-24 23:47:41 -0700

    Fleshing out atoms, adding log4net.

---
+++ LibOpenBYOND/Utils.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+
+namespace OpenBYOND
+{
+    public class Utils
+    {
+        private static Dictionary<string,Type> simplifiableTypes = new Dictionary<string,Type>();
+        private static readonly ILog log = LogManager.GetLogger(typeof(Utils));
+
+        static Utils()
+        {
+            simplifiableTypes.Add("number", typeof(int));
+            simplifiableTypes.Add("float", typeof(float));
+            simplifiableTypes.Add("string", typeof(string));
+        }
+
+        public static object SimplifyProperty(Type type, World.Atom a)
+        {
+            if(type == typeof(int))
+                return AtomToInt(a);
+            if(type == typeof(float))
+                return AtomToFloat(a);
+            if(type == typeof(string))
+                return AtomToString(a);
+            if(type == typeof(World.Atom))
+                return a;
+
+            log.WarnFormat("Unable to simplify type {0}.", type.FullName);
+            return a;
+        }
+
+        private static object AtomToString(World.Atom a)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static object AtomToFloat(World.Atom a)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static object AtomToInt(World.Atom a)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- LibOpenBYOND/World/Atom.cs
+++ LibOpenBYOND/World/Atom.cs
@@ -11,10 +11,100 @@ namespace OpenBYOND.World
     public class Atom
     {
         /// <summary>
+        /// ID of the instance.
+        /// </summary>
+        public int ID;
+
+        /// <summary>
         /// Use the GetProperty function to get this as a simple type.
         /// </summary>
         public Dictionary<string, Atom> Properties = new Dictionary<string, Atom>();
 
+        /// <summary>
+        /// Used for initial().  Copied after compile.
+        /// </summary>
+        public Dictionary<string, Atom> InitialProperties = new Dictionary<string, Atom>();
 
+        public Atom(bool makeInitial=true)
+        {
+            if (makeInitial)
+                InitialProperties = new Dictionary<string,Atom>(Properties); // Make copy.
+        }
+
+        /// <summary>
+        /// Yes, type in BYOND is mutable.  How fucking dumb.
+        /// </summary>
+        public string BYONDType { get; set; }
+
+        public T GetProperty<T>(string key)
+        {
+            Atom a = null;
+            if (!Properties.TryGetValue(key, out a))
+            {
+                throw new KeyNotFoundException(string.Format("Property {0} does not exist in {1}.", key, GetName()));
+            }
+            return (T)Utils.SimplifyProperty(typeof(T), a);
+        }
+
+        public T GetProperty<T>(string key, T defaultValue)
+        {
+            Atom a = null;
+            if (!Properties.TryGetValue(key, out a))
+            {
+                return defaultValue;
+            }
+            return (T)Utils.SimplifyProperty(typeof(T), a);
+        }
+
+        public void SetProperty<T>(string key, T val)
+        {
+            Atom a = null;
+            if(val.GetType() == typeof(Atom))
+            {
+                object o = val;
+                a = (Atom)o;
+            } else {
+                a = new SimpleAtom<T>(val);
+            }
+
+            Properties[key] = a;
+        }
+
+        public string GetName()
+        {
+            // Emulate BYOND behavior.  (ID instead of memory address)
+            // TODO: Proper/improper crap.
+            return GetProperty<string>("name", String.Format("[0x{0:X}]",ID.ToString("X"))); 
+        }
+
+        public override string ToString()
+        {
+            return GetName();
+        }
+    }
+
+    /// <summary>
+    /// For simple types (T = int,float,string)
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class SimpleAtom<T> : Atom {
+
+        public SimpleAtom(T val)
+        {
+            this.Value = val;
+        }
+        /// <summary>
+        /// Value of the "simple" type.
+        /// </summary>
+        public T Value { get; set; }
+
+        /// <summary>
+        /// Implicit casting.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static implicit operator T(SimpleAtom<T> a) {
+            return a.Value;
+        }
     }
 }
