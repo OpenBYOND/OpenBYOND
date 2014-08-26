e7dbba1c069782e2c3cbeb2465c7ef9fe6db2545
Rob Nelson <nexisentertainment@gmail.com>
2014-08-25 17:04:06 -0700

    Add unit tests for VS2010.

---
+++ LibOpenBYOND/DME.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using log4net;
+
+namespace OpenBYOND
+{
+    /// <summary>
+    /// Class for storing which files are loaded in a project.
+    /// </summary>
+    public class DME
+    {
+        public List<string> Files = new List<string>();
+
+        private string rootDir = "";
+        private static readonly ILog log = LogManager.GetLogger(typeof(Utils));
+
+        /// <summary>
+        /// Load a DME file.
+        /// </summary>
+        /// <param name="filename">DME file to load.</param>
+        public void LoadFile(string filename)
+        {
+            rootDir = Path.GetDirectoryName(filename);
+            if (!File.Exists(filename))
+            {
+                throw new FileNotFoundException(String.Format("DME {0} not found.", filename));
+            }
+            log.DebugFormat("Loading {0} ({1})...", filename, rootDir);
+            using (StreamReader rdr = File.OpenText(filename))
+            {
+                string line;
+                while (rdr.Peek() >= 0)
+                {
+                    line = rdr.ReadLine();
+                    if (line.StartsWith("#include"))
+                        parseInclude(line);
+                }
+            }
+            log.DebugFormat("Loaded {0} files.", Files.Count);
+        }
+
+        private void parseInclude(string line)
+        {
+            bool inString = false;
+            string filename = "";
+            foreach (char c in line)
+            {
+                if (c == '"')
+                {
+                    inString = !inString;
+                    if (!inString)
+                    {
+                        string filepath = Path.Combine(rootDir, filename.Replace('\\', Path.DirectorySeparatorChar));
+                        filepath = Path.GetFullPath(filepath);
+                        log.DebugFormat("Added file {0} to DME.", filepath);
+                        Files.Add(filepath);
+                        filename = "";
+                    }
+                    continue;
+                }
+                else
+                {
+                    if (inString)
+                        filename += c;
+                }
+            }
+        }
+    }
+}
---
+++ Test/DMETest.cs
@@ -0,0 +1,109 @@
+﻿using OpenBYOND;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Test
+{
+
+
+    /// <summary>
+    ///This is a test class for DMETest and is intended
+    ///to contain all DMETest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class DMETest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for LoadFile
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("TestFiles","TestFiles")]
+        public void LoadFileTest()
+        {
+            testContextInstance.WriteLine("CD: {0}", Environment.CurrentDirectory);
+
+            DME target = new DME();
+            string filename = Path.Combine("TestFiles","Test.dme");
+
+            List<string> expected = new List<string>();
+            expected.Add(Path.GetFullPath(Path.Combine("TestFiles", "code", "global.dm")));
+            expected.Add(Path.GetFullPath(Path.Combine("TestFiles", "code", "world.dm")));
+            expected.Add(Path.GetFullPath(Path.Combine("TestFiles", "interface", "interface.dm")));
+            expected.Add(Path.GetFullPath(Path.Combine("TestFiles", "interface", "skin.dmf")));
+            expected.Add(Path.GetFullPath(Path.Combine("TestFiles", "maps", "tgstation.dmm")));
+
+            target.LoadFile(filename);
+
+            Assert.AreEqual(target.Files.Count, 5, "Got incorrect # of files.");
+            CollectionAssert.AllItemsAreInstancesOfType(target.Files, typeof(string), "Wrong types.");
+            CollectionAssert.AreEqual(expected, target.Files, "Wrong entries");
+        }
+
+        /// <summary>
+        ///A test for parseInclude
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("LibOpenBYOND.dll")]
+        public void parseIncludeTest()
+        {
+            DME_Accessor target = new DME_Accessor(); // TODO: Initialize to an appropriate value
+            string line = "#include \"balls.txt\""; // TODO: Initialize to an appropriate value
+            string expected = Path.GetFullPath(Path.Combine("balls.txt"));
+            target.parseInclude(line);
+            CollectionAssert.Contains(target.Files, expected, "Balls.txt not in Files.");
+        }
+    }
+}
