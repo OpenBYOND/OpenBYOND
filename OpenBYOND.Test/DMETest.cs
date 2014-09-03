74bf66e4cb59caf22d3034c32914ca1202d0c53a
Rob Nelson <nexisentertainment@gmail.com>
2014-09-03 14:21:27 -0700

    More unit test fixes.

--- OpenBYOND.Test/DMETest.cs
+++ OpenBYOND.Test/DMETest.cs
@@ -1,70 +1,11 @@
-﻿using OpenBYOND;
-using NUnit.Framework;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
+using NUnit.Framework;
 
 namespace OpenBYOND.Test
 {
-
-
-    /// <summary>
-    ///This is a test class for DMETest and is intended
-    ///to contain all DMETest Unit Tests
-    ///</summary>
     public class DMETest
     {
-
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        // 
-        //You can use the following additional attributes as you write your tests:
-        //
-        //Use ClassInitialize to run code before running the first test in the class
-        //[ClassInitialize()]
-        //public static void MyClassInitialize(TestContext testContext)
-        //{
-        //}
-        //
-        //Use ClassCleanup to run code after all tests in a class have run
-        //[ClassCleanup()]
-        //public static void MyClassCleanup()
-        //{
-        //}
-        //
-        //Use TestInitialize to run code before running each test
-        //[TestInitialize()]
-        //public void MyTestInitialize()
-        //{
-        //}
-        //
-        //Use TestCleanup to run code after each test has run
-        //[TestCleanup()]
-        //public void MyTestCleanup()
-        //{
-        //}
-        //
-        #endregion
-
-
         /// <summary>
         ///A test for LoadFile
         ///</summary>
