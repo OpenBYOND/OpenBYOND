8f9321c26eb863fb4fb7571a17671f948ce13414
Rob Nelson <nexisentertainment@gmail.com>
2014-10-13 16:55:31 -0700

    I am a dumb.

--- OpenBYOND.Test/DMMTest.cs
+++ OpenBYOND.Test/DMMTest.cs
@@ -20,7 +20,7 @@ namespace OpenBYOND.Test
             OpenBYOND.World.World world = new OpenBYOND.World.World();
             target.Load(world, filename);
 
-            Assert.AreEqual(5, world.Levels.Count);
+            Assert.AreEqual(6, world.Levels.Count);
 
             ZLevel zlev = world.Levels[0];
             Assert.AreEqual(255, zlev.Height);
